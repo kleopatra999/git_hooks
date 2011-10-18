785726f770882d583402c664d180fc9cb6f29ed9
Potapov Sergey <blake131313@gmail.com>
2011-10-18 13:03:31 +0300

    Improved roodi test Task 1287

--- .git_hooks/tests/pre-commit_roodi_test.bash
+++ .git_hooks/tests/pre-commit_roodi_test.bash
@@ -6,8 +6,12 @@ USE_HOOKS="roodi"
 test_roodi(){
     echo "class User; def initialize; @@var += 1; end; end" > ./user.rb
     git add -A ./user.rb
-    warning=$(git commit -m "Created user" 2>&1 1>/dev/null)
+    local warning=$(git commit -m "Created user to test roodi" 2>&1 1>/dev/null)
+    # verify warning
     assertEquals "user.rb:1 - Don't use class variables. You might want to try a different design." "$warning"
+    # verify the change was commited
+    local last_msg=$(git log --format='%s' -n 1)
+    assertEquals "Created user to test roodi" "$last_msg"
 }
 
 . ./shunit2
